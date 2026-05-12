c08c56c2723b7973cb4537d09480bca4ae19ccb5
zpect-dev <jvbertaggia@gmail.com>
2026-05-11 23:36:41 -0300

    eliminadas columnas innecesarias, claves foraneas añadidas

--- db/migrations/000008_create_desc_ptc_desc_prov.up.sql
+++ db/migrations/000008_create_desc_ptc_desc_prov.up.sql
@@ -11,21 +11,20 @@ CREATE TABLE IF NOT EXISTS desc_ptc (
     porc3    DECIMAL(18,5)  DEFAULT 0,
     porc4    DECIMAL(18,5)  DEFAULT 0,
     porc5    DECIMAL(18,5)  DEFAULT 0,
-    PRIMARY KEY (co_prov, tipo_cli)
+
+    PRIMARY KEY (co_prov, tipo_cli),
+
+    CONSTRAINT fk_desc_ptc_cat_art FOREIGN KEY (co_prov) REFERENCES cat_art(co_cat) ON DELETE RESTRICT,
+    CONSTRAINT fk_desc_ptc_tipo_cli FOREIGN KEY (tipo_cli) REFERENCES tipo_cli(tip_cli) ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS desc_prov (
     co_prov  VARCHAR(30)    NOT NULL,
     co_cli   VARCHAR(50)    NOT NULL,
-    hasta1   DECIMAL(18,5)  DEFAULT 0,
-    hasta2   DECIMAL(18,5)  DEFAULT 0,
-    hasta3   DECIMAL(18,5)  DEFAULT 0,
-    hasta4   DECIMAL(18,5)  DEFAULT 0,
-    hasta5   DECIMAL(18,5)  DEFAULT 0,
     porc1    DECIMAL(18,5)  DEFAULT 0,
-    porc2    DECIMAL(18,5)  DEFAULT 0,
-    porc3    DECIMAL(18,5)  DEFAULT 0,
-    porc4    DECIMAL(18,5)  DEFAULT 0,
-    porc5    DECIMAL(18,5)  DEFAULT 0,
-    PRIMARY KEY (co_prov, co_cli)
+
+    PRIMARY KEY (co_prov, co_cli),
+
+    CONSTRAINT fk_desc_prov_cat_art FOREIGN KEY (co_prov) REFERENCES cat_art(co_cat) ON DELETE RESTRICT,
+    CONSTRAINT fk_desc_prov_clientes FOREIGN KEY (co_cli) REFERENCES clientes(co_cli) ON DELETE RESTRICT
 );
